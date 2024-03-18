807350eeb2d76a7db23c195db1ae5862547eb864
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-03-18 14:56:02 +0700

    ci: fix deployment false possible

--- common/scripts/index.sh
+++ common/scripts/index.sh
@@ -115,26 +115,3 @@ _log() {
 #######################################################
 
 _main "$@"
-
-# _GH_ACTIONS="${ACTIONS_TARGET_DIRECTORY:?}"
-# _GH_WORKFLOW="$_GH_ACTIONS/.github/workflows"
-
-# ! test -d "$_GH_WORKFLOW" && mkdir -p "$_GH_WORKFLOW"
-
-# ## Copy workflows and resources
-# cp -r "$_ROOT/resources/." "$_GH_ACTIONS"
-# cp -r "$_ROOT/workflows/." "$_GH_WORKFLOW"
-
-# cd "$_ROOT/$_GH_ACTIONS" || exit 1
-
-# printf 'deploying '%s'\n' "$PWD"
-# git status
-# ## Deployment only if file changes
-# if ! git diff --exit-code --quiet || test -n "$ACTIONS_FORCE_MODE"; then
-#   git config --local user.name "$ACTIONS_COMMIT_NAME"
-#   git config --local user.email "$ACTIONS_COMMIT_EMAIL"
-
-#   git add -A
-#   git commit -m "$ACTIONS_COMMIT_MSG"
-#   git push origin "$ACTIONS_TARGET_BRANCH"
-# fi
