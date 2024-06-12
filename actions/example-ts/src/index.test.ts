f5f845f891e0301a0019bde5cf7943bb62005270
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-06-12 17:49:52 +0700

    fix: test fail

--- actions/example-ts/src/index.test.ts
+++ actions/example-ts/src/index.test.ts
@@ -8,7 +8,7 @@ import runner from "./index"
 describe("application runner", () => {
   test("should print info logs", async () => {
     await runner({ input: { name: "name" } }, context)
-    expect(info).toHaveBeenCalledTimes(2)
+    expect(info).toHaveBeenCalled()
     expect(info).toHaveBeenLastCalledWith("hello name")
   })
 })
--- actions/example-ts/src/index.ts
+++ actions/example-ts/src/index.ts
@@ -5,10 +5,10 @@ import actions from "./app"
 const runner: AppRunner<typeof actions> = (data, context) => {
   const logger = context.use("log")
 
-  logger.info("Use {0}: {1}", context.name, context.version)
-  logger.info("hello {name}", data.input)
   logger.info(process.cwd())
   logger.info(__dirname)
+  logger.info("Use {0}: {1}", context.name, context.version)
+  logger.info("hello {name}", data.input)
 }
 
 actions.exec(runner)
