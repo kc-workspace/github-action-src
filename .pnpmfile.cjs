7c04701848c2ccb22d119f1d032007727272a0a4
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-06-14 15:17:18 +0700

    chore: remove direct dependencies to @actions/* packages

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,6 +1,5 @@
 function readPackage(pkg, context) {
   if (typeof pkg.name === "string" && pkg.name.startsWith("@actions/")) {
-    context.log("found @actions/* package");
     pkg["sideEffects"] = false;
   }
 
